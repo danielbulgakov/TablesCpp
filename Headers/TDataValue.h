c0a5823237202e46e394117f920d18b3b78dcd69
danielbulgakov <bulgach09@gmail.com>
2022-04-27 13:44:24 +0300

    TTabRecord declaration added

--- Headers/TDataValue.h
+++ Headers/TDataValue.h
@@ -1,10 +1,17 @@
 #pragma once
+#include <iostream>
 
 class TDataValue{
 public:
+    virtual void Print(ostream& where) const = 0;
     virtual TDataValue* GetCopy() = 0;
     ~TDataValue(){};
 
+    friend std::ostream& operator<<(std::ostream& out, const TDataValue& data ){
+        data.Print(out);
+        return out;
+    }
+    
 };
 
 typedef TDataValue* PTDataValue;
--- Headers/TTabRecord.h
+++ Headers/TTabRecord.h
@@ -1,4 +1,63 @@
 #ifndef _TTABRECORD_
 #define _TTABRECORD_
 
+#include <string>
+#include <iostream>
+#include "TDataValue.h"
+
+typedef std::string TKey;
+typedef TDataValue* PTDataValue;
+
+class TTabRecord;
+typedef TTabRecord* PTTabRecord;
+
+class TTabRecord : TDataValue
+{
+protected:
+    TKey Key;
+    PTDataValue pValue;
+
+    // virtual void Print(std::ostream& where) { where << Key << "   " << pValue;}
+
+public:
+
+    TTabRecord(TKey key, PTDataValue pvalue) : Key(key), pValue(pvalue) {}
+
+    void SetKet(TKey key) {this->Key = key;}
+    TKey GetKet(TKey key) {return this->Key;}
+    void SetValue(PTDataValue pvalue){this->pValue = pvalue;}
+    PTDataValue GetValue (){return this->pValue;}
+    // PTDataValue GetCopy() {return new TDataValue(this->Key, this->pValue);}
+
+    // TTabRecord operator= (const TTabRecord& tr){  Cant return abstract class object
+    //     this->Key = tr.Key;
+    //     this->pValue = tr.pValue; // GetCopy() ? SetValue()?
+    //     return *this;
+    // }
+
+    virtual bool operator==(const TTabRecord& tr) const{
+        return this->Key == tr.Key;
+    } // 5 > 3 ? true : false
+
+    virtual bool operator> (const TTabRecord& tr) const{
+        return this->Key > tr.Key;
+    }
+
+    virtual bool operator< (const TTabRecord& tr) const{
+        return this->Key < tr.Key; 
+    }
+
+    friend class TArrayTable;
+    friend class TScanTable;
+    friend class TSortTable;
+    friend class TTreeTable;
+    friend class TTreeNode;
+    friend class TArrayHashTable;
+    friend class TListHashTable;
+
+
+
+};
+
+
 #endif //_TTABRECORD_
