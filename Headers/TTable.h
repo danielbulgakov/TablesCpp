ca3fa44ed0f6f7d5441037bde9238248b8097d85
danielbulgakov <bulgach09@gmail.com>
2022-04-28 14:38:53 +0300

    TTable class added

--- Headers/TTable.h
+++ Headers/TTable.h
@@ -1,4 +1,41 @@
 #ifndef _TTABLE_
 #define _TTABLE_
-class TTable;
+#include "TDataCom.h"
+#include "TDataValue.h"
+#include "TTabRecord.h"
+#include <iostream>
+
+class TTable : TDataCom{
+protected:
+    size_t DataCount;
+    int32_t Efficiency;
+
+public:
+    TTable() : DataCount(0) , Efficiency(0) {}
+    virtual ~TTable();
+
+    size_t GetDataCount() const {return DataCount;}
+    int32_t GetEfficiency() const {return Efficiency;}
+    bool isEmpty() const {return DataCount <= 0;}
+
+    virtual bool isFull() const = 0;
+    virtual PTDataValue FindRecord(TKey key) = 0;
+    virtual void InsRecord (TKey key, PTDataValue pvalue) = 0;
+    virtual void DelRecord (TKey key) = 0;
+
+    virtual int32_t Reset() = 0;
+    virtual int32_t GoNext() = 0;
+    virtual bool isTabEnded() = 0;
+
+    virtual TKey GetKey() const = 0;
+    virtual PTDataValue GetValue() const = 0;
+
+    friend std::ostream& operator<< (std::ostream& out, TTable& t){
+        for (t.Reset(); !t.isTabEnded(); t.GoNext()){
+            out << "Key " << t.GetKey() << " Val " << t.GetValue();
+        }
+        return out;
+    }
+};
+
 #endif // _TTABLE_
